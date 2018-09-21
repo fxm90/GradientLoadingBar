4a21aeeb84e41d10dbdbea8bb15b6ad5099f31d7
Michael Rothkegel <michael.rothkegel@invers.com>
2018-09-21 10:35:51 +0200

    -small refactoring for swift 4.2 -update example to swift 4.2

--- GradientLoadingBar/Classes/Views/GradientView.swift
+++ GradientLoadingBar/Classes/Views/GradientView.swift
@@ -93,7 +93,7 @@ public final class GradientView: UIView {
 
         animation.duration = duration
 
-        animation.fillMode = kCAFillModeForwards
+        animation.fillMode = CAMediaTimingFillMode.forwards
         animation.isRemovedOnCompletion = false
 
         gradientLayer.add(animation, forKey: animationKey)
