03bf358f0da602c3a02c56d20dc99000d078818b
Emil Bellmann <emilrbellmann@gmail.com>
2019-03-29 14:00:44 -0300

    Made extension methods interal to avoid conflicts with other frameworks

--- GradientLoadingBar/Classes/Extensions/UIColor/UIColor+CustomColors.swift
+++ GradientLoadingBar/Classes/Extensions/UIColor/UIColor+CustomColors.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIColor {
     /// Struct that contains all our custom defined colors.
-    struct CustomColors {
+    internal struct CustomColors {
         static let grey = #colorLiteral(red: 0.8980392157, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
         static let green = #colorLiteral(red: 0.2980392157, green: 0.8509803922, blue: 0.3921568627, alpha: 1)
         static let violet = #colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1)
