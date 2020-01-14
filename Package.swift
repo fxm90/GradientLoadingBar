9ab3a6987aa2872ff86c45e50ef8308fccc7eb51
Artyom Rudakov <two_dollars@icloud.com>
2020-01-14 19:26:36 +0200

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GradientLoadingBar",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "GradientLoadingBar",
+            targets: ["GradientLoadingBar"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/fxm90/LightweightObservable",
+            .upToNextMajor(from: "2.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "GradientLoadingBar",
+            dependencies: ["LightweightObservable"],
+            path: "GradientLoadingBar/Classes"
+        )
+    ]
+)
