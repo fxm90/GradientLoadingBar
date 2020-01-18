43bb14de88dc95a8c92fdf8e56fd3dcbb3c20f5e
Felix Mau <felix.mau@moia.io>
2020-01-18 09:41:35 +0100

    🎨 :: Format `Package.swift` with `SwiftFormat` to fix build

--- Package.swift
+++ Package.swift
@@ -2,26 +2,18 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "GradientLoadingBar",
-    platforms: [.iOS(.v9)],
-    products: [
-        .library(
-            name: "GradientLoadingBar",
-            targets: ["GradientLoadingBar"]
-        )
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/fxm90/LightweightObservable",
-            .upToNextMajor(from: "2.0.0")
-        )
-    ],
-    targets: [
-        .target(
-            name: "GradientLoadingBar",
-            dependencies: ["LightweightObservable"],
-            path: "GradientLoadingBar/Classes"
-        )
-    ]
-)
+let package = Package(name: "GradientLoadingBar",
+                      platforms: [.iOS(.v9)],
+                      products: [
+                          .library(name: "GradientLoadingBar",
+                                   targets: ["GradientLoadingBar"])
+                      ],
+                      dependencies: [
+                          .package(url: "https://github.com/fxm90/LightweightObservable",
+                                   .upToNextMajor(from: "2.0.0"))
+                      ],
+                      targets: [
+                          .target(name: "GradientLoadingBar",
+                                  dependencies: ["LightweightObservable"],
+                                  path: "GradientLoadingBar/Classes")
+                      ])
