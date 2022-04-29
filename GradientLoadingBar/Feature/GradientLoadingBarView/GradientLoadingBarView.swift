54a413d9868e75838af216d534c07fcfed449b3d
Felix Mau <me@felix.hamburg>
2022-04-29 18:40:10 +0100

    🐛 :: Fix `xcodebuild` errors when running `pod lib lint`

--- GradientLoadingBar/Feature/GradientLoadingBarView/GradientLoadingBarView.swift
+++ GradientLoadingBar/Feature/GradientLoadingBarView/GradientLoadingBarView.swift
@@ -8,74 +8,88 @@
 
 import SwiftUI
 
-// For some reason the animation looks broken on iOS versions <= 15.0.
-@available(iOS 15.0, *)
-public struct GradientLoadingBarView: View {
+// Workaround to fix `xcodebuild` errors when running `pod lib lint`, like e.g.:
+//
+// - `xcodebuild`: error: cannot find type 'Color' in scope
+// - `xcodebuild`: error: cannot find type 'View' in scope
+//
+// > This failure occurs because builds with a deployment target earlier than iOS 11 will also build for the armv7 architecture,
+// > and there is no armv7 swiftmodule for SwiftUI in the iOS SDK because the OS version in which it was first introduced (iOS 13)
+// > does not support armv7 anymore.
+//
+// Source: https://stackoverflow.com/a/61954608
+#if arch(arm64) || arch(x86_64)
 
-    // MARK: - Config
+    // For some reason the animation looks broken on iOS versions <= 15.0.
+    @available(iOS 15.0, *)
+    public struct GradientLoadingBarView: View {
 
-    public enum Config {
-        /// The default color palette for the gradient colors.
-        public static let gradientColors = UIColor.GradientLoadingBar.gradientColors.map(Color.init)
+        // MARK: - Config
 
-        /// The default duration for the progress animation, measured in seconds.
-        public static let progressDuration = TimeInterval.GradientLoadingBar.progressDuration
-    }
+        public enum Config {
+            /// The default color palette for the gradient colors.
+            public static let gradientColors = UIColor.GradientLoadingBar.gradientColors.map(Color.init)
 
-    // MARK: - Private properties
+            /// The default duration for the progress animation, measured in seconds.
+            public static let progressDuration = TimeInterval.GradientLoadingBar.progressDuration
+        }
 
-    @StateObject
-    private var viewModel: ViewModel
+        // MARK: - Private properties
 
-    // MARK: - Instance Lifecycle
+        @StateObject
+        private var viewModel: ViewModel
 
-    public init(gradientColors: [Color] = Config.gradientColors,
-                progressDuration: TimeInterval = Config.progressDuration) {
-        // Even though the docs mention that "You don’t call this initializer directly", this seems to be the correct way to set-up a
-        // `StateObject` with parameters according to "Lessons from the SwiftUI Digital Lounge".
-        // https://swiftui-lab.com/random-lessons/#data-10
-        _viewModel = StateObject(
-            wrappedValue: ViewModel(gradientColors: gradientColors, progressDuration: progressDuration)
-        )
-    }
+        // MARK: - Instance Lifecycle
 
-    // MARK: - Render
-
-    public var body: some View {
-        Color.clear
-            // We explicitly have to use a `PreferenceKey` here and store the size on a property in order to restart the animation whenever
-            // the size changes. Using a `GeometryReader` together with the `onAppear(_:)` view-modifier doesn't reflect any size changes.
-            .modifier(SizeModifier())
-            .onPreferenceChange(SizePreferenceKey.self) {
-                viewModel.size = $0
-            }
-            // Using an `overlay` here makes sure that the parent view won't change it's frame.
-            .overlay(
-                LinearGradient(colors: viewModel.gradientColors, startPoint: .leading, endPoint: .trailing)
-                    .frame(width: viewModel.gradientWidth)
-                    .offset(x: viewModel.horizontalOffset, y: 0)
+        public init(gradientColors: [Color] = Config.gradientColors,
+                    progressDuration: TimeInterval = Config.progressDuration) {
+            // Even though the docs mention that "You don’t call this initializer directly", this seems to be the correct way to set-up a
+            // `StateObject` with parameters according to "Lessons from the SwiftUI Digital Lounge".
+            // https://swiftui-lab.com/random-lessons/#data-10
+            _viewModel = StateObject(
+                wrappedValue: ViewModel(gradientColors: gradientColors, progressDuration: progressDuration)
             )
+        }
+
+        // MARK: - Render
+
+        public var body: some View {
+            Color.clear
+                // We explicitly have to use a `PreferenceKey` here and store the size on a property in order to restart the animation whenever
+                // the size changes. Using a `GeometryReader` together with the `onAppear(_:)` view-modifier doesn't reflect any size changes.
+                .modifier(SizeModifier())
+                .onPreferenceChange(SizePreferenceKey.self) {
+                    viewModel.size = $0
+                }
+                // Using an `overlay` here makes sure that the parent view won't change it's frame.
+                .overlay(
+                    LinearGradient(colors: viewModel.gradientColors, startPoint: .leading, endPoint: .trailing)
+                        .frame(width: viewModel.gradientWidth)
+                        .offset(x: viewModel.horizontalOffset, y: 0)
+                )
+        }
     }
-}
 
-// MARK: - Helper
+    // MARK: - Helper
 
-private struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
+    private struct SizePreferenceKey: PreferenceKey {
+        static var defaultValue: CGSize = .zero
 
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        value = nextValue()
+        static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+            value = nextValue()
+        }
     }
-}
 
-@available(iOS 15.0, *)
-private struct SizeModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content.background(
-            GeometryReader { geometry in
-                Color.clear.preference(key: SizePreferenceKey.self,
-                                       value: geometry.size)
-            }
-        )
+    @available(iOS 15.0, *)
+    private struct SizeModifier: ViewModifier {
+        func body(content: Content) -> some View {
+            content.background(
+                GeometryReader { geometry in
+                    Color.clear.preference(key: SizePreferenceKey.self,
+                                           value: geometry.size)
+                }
+            )
+        }
     }
-}
+
+#endif
